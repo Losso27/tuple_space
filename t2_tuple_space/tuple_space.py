651302c452540bedbd3a3ff295d15eb3a8a81a54
losso27 <felipe_losso@hotmail.com>
2024-06-28 13:43:58 -0300

    Fix tuples

--- t2_tuple_space/tuple_space.py
+++ t2_tuple_space/tuple_space.py
@@ -1,9 +1,10 @@
-from pysyncobj import SyncObj, replicated
+from pysyncobj import SyncObj, SyncObjConf, replicated
 
 
 class TupleSpace(SyncObj):
     def __init__(self, self_node_address, partners_node_addresses):
-        super(TupleSpace, self).__init__(self_node_address, partners_node_addresses)
+        cfg = SyncObjConf(dynamicMembershipChange = True)
+        super(TupleSpace, self).__init__(self_node_address, partners_node_addresses, cfg)
         self.space = []
         print("self:", self_node_address)
         print("partners:", partners_node_addresses)
@@ -12,25 +13,32 @@ class TupleSpace(SyncObj):
         future_tuple = []
         for x in enumerate(t):
             if x[0] in i:
+                future_tuple.append("*")
+            else:
                 future_tuple.append(x[1])
         return tuple(future_tuple)
 
+
     def read(self, t):
-        itens = []
+        generic_item = []
         for x in enumerate(t):
-            if x[1] != "*":
-                itens.append(x[0])
-        t2 = self.adjust_tuple(t, itens)
+            if x[1] == "*":
+                generic_item.append(x[0])
         for s in self.space:
-            if self.adjust_tuple(s, itens) == t2:
+            if self.adjust_tuple(s, generic_item) == t:
                 return s
 
     @replicated
     def write(self, t):
         self.space.append(t)
+        
+    @replicated
+    def remove(self, t):
+        self.space.remove(t)
 
     @replicated
     def get(self, t):
         t2 = self.read(t)
-        if t2 is not None:
+        if t2 != None:
             self.space.remove(t2)
+        return t2
